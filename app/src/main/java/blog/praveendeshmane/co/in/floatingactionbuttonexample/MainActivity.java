fc747d3c96e437ba33aa1e97c43b8780e459255f
praveendeshmane <deshmane.praveen@gmail.com>
2017-12-14 00:08:35 +0530

    Initial commit

---
+++ app/src/main/java/blog/praveendeshmane/co/in/floatingactionbuttonexample/MainActivity.java
@@ -0,0 +1,30 @@
+package blog.praveendeshmane.co.in.floatingactionbuttonexample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NewMessageActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
